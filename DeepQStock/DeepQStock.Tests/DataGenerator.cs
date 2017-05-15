f6829273ae0986824f8bb23ccef80bfa7362d87c
jcaramello <jose.caramello@assertia.com.ar>
2017-05-15 12:00:56 -0300

    add test project

---
+++ DeepQStock/DeepQStock.Tests/DataGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepQStock.Tests
+{
+    public static class DataGenerator
+    {
+        /// <summary>
+        /// Gets the sample periods.
+        /// </summary>
+        /// <returns></returns>
+        public static IList<Period> GetSamplePeriods()
+        {
+            return new List<Period>()
+            {
+                new Period() { Close = 22.27 },
+                new Period() { Close = 22.19 },
+                new Period() { Close = 22.08 },
+                new Period() { Close = 22.17 },
+                new Period() { Close = 22.18 },
+                new Period() { Close = 22.13 },
+                new Period() { Close = 22.23 },
+                new Period() { Close = 22.43 },
+                new Period() { Close = 22.24 },
+                new Period() { Close = 22.29 },
+                new Period() { Close = 22.15 }
+            };
+        }
+    }
+}
---
+++ DeepQStock/DeepQStock.Tests/IndicatorsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeepQStock.Indicators;
+using System.Linq;
+
+namespace DeepQStock.Tests
+{
+    [TestClass]
+    public class IndicatorsTests : BaseTest
+    {       
+        #region  << Tests >>
+
+        [TestMethod]
+        public void MovingAverage_Test()
+        {
+            var sma = new SimpleMovingAverage(10);
+            var ema = new ExponentialMovingAverage(10);
+            var periods = DataGenerator.GetSamplePeriods();
+            var sma_value = 0.0;
+            var ema_value = 0.0;
+
+            foreach (var p in periods)
+            {
+                sma_value = sma.Calculate(p).First();                
+                ema_value = ema.Calculate(p).First();
+            }
+
+            Assert.AreEqual(sma_value, 22.209);
+            Assert.AreEqual(Math.Round(ema_value, 2), 22.22);
+        }
+
+        #endregion
+    }
+}
